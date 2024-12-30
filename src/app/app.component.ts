f97098198da04e6150e9f407f957e2b119ebc462
Camilo <yugihor@hotmail.com>
2024-12-30 15:57:14 -0500

    Cambio contraseña en app component, mejora boton cerrar sesion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,21 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { Router } from '@angular/router';
-
+import { ChangePasswordModalComponent } from './modals/modal-pass-change/pass.change.component';
+import  { NzModalComponent } from 'ng-zorro-antd/modal';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule],
+  imports: [CommonModule, RouterLink, 
+    RouterOutlet, NzModalComponent,
+    NzIconModule, NzLayoutModule, 
+    NzMenuModule, ChangePasswordModalComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   isCollapsed = false;
+  isChangePasswordModalVisible = false;
 
   constructor(private router: Router) {}
 
@@ -25,5 +30,18 @@ export class AppComponent {
   logout(): void {
     localStorage.removeItem('authToken'); // Elimina el token de autenticación
     this.router.navigate(['/form-login']); // Redirige al login
+  }
+  openChangePasswordModal(): void {
+    this.isChangePasswordModalVisible = true;
+  }
+
+  closeChangePasswordModal(): void {
+    this.isChangePasswordModalVisible = false;
+  }
+  onPasswordChange(data: { oldPassword: string; newPassword: string }): void {
+    console.log('Old Password:', data.oldPassword);
+    console.log('New Password:', data.newPassword);
+    // Implementa aquí la lógica para actualizar la contraseña.
+    this.closeChangePasswordModal();
   }
 }
